228daff50a4964d7c9ccc3aea81a3b28b0af0d6c
iamFate <stephalex222@gmail.com>
2022-08-25 21:29:56 -0700

    added a innerHTML to my js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 const clickMe = document.querySelector('.click');
  clickMe.addEventListener("click", function(){
-   console.log("good Morning")
+   document.querySelector(".h1").innerHTML= "Good Morning Fate"
  })
